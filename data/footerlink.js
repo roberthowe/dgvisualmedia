1bc6fa905909a4f44ce46d55cb4334e69c0b0e89
Bobby Howe <bobbyjhowe@gmail.com>
2025-05-27 16:31:01 +0100

    Update footer links

--- data/footerlink.js
+++ data/footerlink.js
@@ -1,6 +1,6 @@
 export const links = [
-  { id: 1, href: "#", text: "Work" },
-  { id: 2, href: "#", text: "Services" },
-  { id: 3, href: "#", text: "Case Studies" },
-  { id: 4, href: "#", text: "Contact" },
+  { id: 1, href: "/work", text: "Work" },
+  { id: 2, href: "/services", text: "Services" },
+  { id: 3, href: "/blog", text: "Blog" },
+  { id: 4, href: "/contact", text: "Contact" },
 ];
